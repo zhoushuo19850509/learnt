e7c79383214b75e3847ef586e834da2458d56f72
athena <516534774@qq.com>
2017-06-07 09:36:14 +0800

    chap8 finish Gold Challenge:Customizing the Table

--- testGit/Homepwner/Homepwner/BNRItemsViewController.m
+++ testGit/Homepwner/Homepwner/BNRItemsViewController.m
@@ -43,6 +43,8 @@
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell"
                                                      forIndexPath:indexPath];
     NSInteger count = [[[BNRItemStore sharedStore] allTtems] count];
+    
+    // 这个if分支为了处理最后添加上去的一行
     if(indexPath.row == count){
         NSLog(@"reach the end of the table!");
         cell.textLabel.text = @"No more items";
@@ -51,11 +53,34 @@
         BNRItem *item = items[indexPath.row];
         
         cell.textLabel.text = [item description];
+        
+        // 设置字体
+        UIFont *newFont = [UIFont fontWithName:@"Arial" size:20.0];
+        cell.textLabel.font = newFont;
+
     }
+    
+    // 打印各个cell的高度
+    CGFloat f = cell.frame.size.height;
+    NSLog(@"print the height of the cell: %f",f);
     
     return cell;
     
 }
+
+
+// 定制tableViewCell的高度  Chap8 Gold Challenge
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSInteger count = [[[BNRItemStore sharedStore] allTtems] count];
+   
+    if(indexPath.row < count ){
+        return 60;
+    }else{
+        return 44;
+    }
+}
+
 
 // view加载的时候，为tableview注册一个重用的ID
 -(void)viewDidLoad
